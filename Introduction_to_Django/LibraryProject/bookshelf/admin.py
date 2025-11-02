1868d30ca2edefd73c9229d3737fc3f3d8c90fcf
AnastaseNtez <anastasentezi@gmail.com>
2025-11-02 17:12:20 +0100

    feat: Integrated and customized book model with Django Admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+# Define the custom configuration for the Book model
+class BookAdmin(admin.ModelAdmin):
+    # 1. Implement custom configurations to display fields in the list view (list_display)
+    list_display = ('title', 'author', 'publication_year')
+
+    # 2. Configure list filters (list_filter)
+    list_filter = ('author', 'publication_year')
+
+    # 3. Configure search capabilities (search_fields) - searches based on the provided fields
+    search_fields = ('title', 'author')
+
+# Register your model with the custom configuration
+admin.site.register(Book, BookAdmin)
+
