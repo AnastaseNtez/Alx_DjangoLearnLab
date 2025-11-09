98626b53468cb54b12a03d3c2411d545a3fd6490
AnastaseNtez <anastasentezi@gmail.com>
2025-11-09 18:25:04 +0100

    adding templates

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,18 @@
+# relationship_app/urls.py
+
+from django.urls import path
+from . import views
+from .views import LibraryDetailView
+
+urlpatterns = [
+    # URL for Function-Based View: Lists all books
+    # Path: /relationship/books/
+    path('books/', views.list_books, name='list_books'),
+
+    # URL for Class-Based View: Displays a specific library detail
+    # Path: /relationship/library/Central_Library/ 
+    # Use 'slug' (or another field) to look up the library by its name
+    path('library/<str:slug>/', 
+         LibraryDetailView.as_view(), 
+         name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# --- 1. Function-Based View (FBV): List all books ---
+def list_books(request):
+    """Lists all books and their authors using a function-based view."""
+    # Retrieve all Book objects from the database
+    all_books = Book.objects.all().select_related('author')
+    
+    context = {
+        'books': all_books,
+        'view_type': 'Function-Based View'
+    }
+    
+    # Renders the HTML template 'list_books.html'
+    return render(request, 'list_books.html', context)
+
+
+# --- 2. Class-Based View (CBV): Library Detail ---
+class LibraryDetailView(DetailView):
+    """Displays details for a specific library, including its books."""
+    # 1. Specify the model to retrieve the object from
+    model = Library
+    
+    # 2. Specify the name of the template to render
+    template_name = 'library_detail.html'
+    
+    # 3. Specify the context object name used in the template (default is 'object')
+    context_object_name = 'library'
+    
+    # 4. By default, DetailView looks for an object using the 'pk' URL parameter.
+    #    ll use a specific field for lookup in the URLs.
+    #    We don't need to specify this here, but we will in urls.py.
+
+    # Note: The template 'library_detail.html' uses library.books.all, 
+    # which automatically handles the Many-to-Many relationship.
