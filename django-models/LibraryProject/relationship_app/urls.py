509adc0b308af068c03ec567cc448801ccddb5e6
AnastaseNtez <anastasentezi@gmail.com>
2025-11-09 18:55:33 +0100

    fixing bug in templating

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from . import views
-from .views import LibraryDetailView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     # URL for Function-Based View: Lists all books
