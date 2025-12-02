206d8b0459ec591d16767c00f86d5981a2e7cb29
AnastaseNtez <anastasentezi@gmail.com>
2025-12-02 21:58:51 +0100

    adding blog project

---
+++ django_blog/blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# The Post model represents a single blog entry.
+class Post(models.Model):
+    # Field 1: Title of the blog post (required, max length 200)
+    title = models.CharField(max_length=200)
+    
+    # Field 2: The main content/body of the blog post
+    content = models.TextField()
+    
+    # Field 3: Automatically set to the date and time the post was created.
+    published_date = models.DateTimeField(auto_now_add=True)
+    
+    # Field 4: Links the post to the User who wrote it.
+    # on_delete=models.CASCADE ensures that if a User is deleted, all their associated 
+    # posts are also deleted. This is the "multiple posts by a single author" relation.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    # A helpful string representation for the Django Admin and shell
+    def __str__(self):
+        return self.title
+
+    # Optional: Define metadata for the model
+    class Meta:
+        # Orders posts by the published_date in descending order (newest first)
+        ordering = ['-published_date']
