f8bd038107242dd9a1f088c9c224e2bb9d6afd0c
AnastaseNtez <anastasentezi@gmail.com>
2025-12-03 23:08:55 +0100

    adding CRUD on the comment section

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,12 +25,12 @@ urlpatterns = [
     
     
     # Create Comment (Uses the function-based view, linked to the Post's PK)
-    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
 
     # Update Comment (Uses the Comment's PK)
     # The URL pattern for the comment PK must be different from the post PK to ensure the URL resolver works correctly.
-    path('post/<int:pk>comments/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:post_id>comments/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     
     # Delete Comment (Uses the Comment's PK)
-    path('post/<int:pk>/comments/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:post_id>/comments/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
