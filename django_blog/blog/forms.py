240541a72904be1291950a625b8ce5a2afb471f0
AnastaseNtez <anastasentezi@gmail.com>
2025-12-03 20:29:23 +0100

    adding CRUD operations

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,20 +1,26 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
+from django import forms
+from django.contrib.auth.models import User
+# We need to import the Post model to create a ModelForm for it
+from .models import Post 
+# Removed: from .forms import CustomUserCreationForm, PostForm (This caused the recursive import error)
 
-# Get the custom or default User model
-User = get_user_model()
 
-class CustomUserCreationForm(UserCreationForm):
+# Assuming this form is for user registration
+class CustomUserCreationForm(forms.ModelForm):
+    # This structure is commonly used for custom registration forms
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+# Defined the missing PostForm for your Create and Update views
+class PostForm(forms.ModelForm):
     """
-    A custom form for user creation which includes the email field.
+    A form for creating and updating blog posts.
     """
     class Meta:
-        # Use the default Django User model
-        model = User
-        # Include username, email, and password fields
-        fields = ('username', 'email')
-        
-    # Ensure email is required
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['email'].required = True
+        model = Post
+        # We only need the user to input the title and content
+        fields = ['title', 'content']
